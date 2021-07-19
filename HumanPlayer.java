6483aaecd2e134b693b51a487e0556a002d0c77e
Justin <justin.chua@ucalgary.ca>
2021-07-19 15:20:37 -0600

    Partial implementation of HumanPlayer class (getter and setter methods).

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,3 +1,23 @@
 public class HumanPlayer {
-
+	private String playerName;
+	private String stoneColor;
+	private int numOfMoves;
+	private int ranking;
+	
+	public String getPlayerName() {return this.playerName;}
+	
+	public void setPlayerName(String playerName) {this.playerName = playerName;}
+	
+	public String getStoneColor() {return this.stoneColor;}
+	
+	public void setStoneColor(String stoneColor) {this.stoneColor = stoneColor;}
+	
+	public int getNumOfMoves() {return this.numOfMoves;}
+	
+	public void setNumOfMoves(int numOfMoves) {this.numOfMoves = numOfMoves;}
+	
+	public int getRanking() {return this.ranking;}
+	
+	public void setRanking(int ranking) {this.ranking = ranking;}
+	
 }
