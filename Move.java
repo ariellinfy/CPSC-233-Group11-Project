1447fbfba3df8be8f87df1bd467d52e4b8e7ad05
ariellinfy <72408793+ariellinfy@users.noreply.github.com>
2021-07-19 15:10:39 -0600

    Implemented Move class and added a Stone enum class.

--- Move.java
+++ Move.java
@@ -4,5 +4,37 @@
  *
  */
 public class Move {
+	private int row;
+	private int col;
+	private Stone stone;
+	
+	public Move(int row, int col, Stone stone) {
+		this.row = row;
+		this.col = col;
+		this.stone = stone;
+	}
 
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	public Stone getStone() {
+		return stone;
+	}
+
+	public void setStone(Stone stone) {
+		this.stone = stone;
+	}
 }
