697609dbab630b2549daad3438337e052709f56d
ariellinfy <72408793+ariellinfy@users.noreply.github.com>
2021-07-21 22:25:33 -0600

    Reviewed and added javadoc to Move class

--- Move.java
+++ Move.java
@@ -1,40 +1,48 @@
 /**
+ * The class contains data needed for a single move in a game.
+ * Setter methods are not provided in the class, since a Move object
+ * is not allowed to be mutated after initialization.
  * 
  * @author Fu-Yin Lin
- *
  */
 public class Move {
 	private int row;
 	private int col;
 	private Stone stone;
 	
+	/**
+	 * Initialize a Move object with its position and type on the board.
+	 * @param row the row index number of the board.
+	 * @param col the column index number of the board.
+	 * @param stone the stone color of this move (either black or white).
+	 */
 	public Move(int row, int col, Stone stone) {
 		this.row = row;
 		this.col = col;
 		this.stone = stone;
 	}
 
+	/**
+	 * Get row index of this move.
+	 * @return the move's index number on the vertical axis of the board.
+	 */
 	public int getRow() {
 		return row;
 	}
 
-	public void setRow(int row) {
-		this.row = row;
-	}
-
+	/**
+	 * Get column index of this move.
+	 * @return the move's index number on the horizontal axis of the board.
+	 */
 	public int getCol() {
 		return col;
 	}
 
-	public void setCol(int col) {
-		this.col = col;
-	}
-
+	/**
+	 * Get stone color of this move.
+	 * @return the move's Stone type on the board.
+	 */
 	public Stone getStone() {
 		return stone;
-	}
-
-	public void setStone(Stone stone) {
-		this.stone = stone;
 	}
 }
